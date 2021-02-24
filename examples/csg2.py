cd734f3dd17e1073e9708ad3dd75b9130182b2c6
brgcode <vanmelet@ethz.ch>
2021-02-24 09:07:00 +0100

    adding CSG model

--- examples/csg2.py
+++ examples/csg2.py
@@ -9,14 +9,12 @@ from compas.geometry import Cylinder
 from compas.geometry import Box
 from compas.geometry import Torus
 from compas.geometry import Capsule
-from compas.geometry import Frame
 
 from compas.datastructures import Mesh
 from compas.utilities import rgb_to_hex
 
 from compas_view2.app import App
 from compas_gmsh.model import Model
-
 
 # ==============================================================================
 # Geometry
@@ -32,37 +30,30 @@ YZ = Plane(O, X)
 ZX = Plane(O, Y)
 XY = Plane(O, Z)
 
-# box = Box.from_width_height_depth(2 * R, 2 * R, 2 * R)
-# sphere = Sphere(O, 1.25 * R)
+box = Box.from_width_height_depth(2 * R, 2 * R, 2 * R)
+sphere = Sphere(O, 1.25 * R)
 
-# cylinderx = Cylinder((YZ, 0.7 * R), 4 * R)
-# cylindery = Cylinder((ZX, 0.7 * R), 4 * R)
-# cylinderz = Cylinder((XY, 0.7 * R), 4 * R)
-
-b1 = Box(Frame([+4, +4, 0], [1, 0, 0], [0, 1, 0]), 10, 10, 10)
-b2 = Box(Frame([-4, -4, 0], [1, 0, 0], [0, 1, 0]), 10, 10, 10)
-
-# a = Mesh.from_shape(b1)
-# b = Mesh.from_shape(b2)
+cylinderx = Cylinder((YZ, 0.7 * R), 4 * R)
+cylindery = Cylinder((ZX, 0.7 * R), 4 * R)
+cylinderz = Cylinder((XY, 0.7 * R), 4 * R)
 
 # ==============================================================================
 # Solid Model
 # ==============================================================================
 
 model = Model(name="boolean")
-model.length_min = 0.2
-model.length_max = 0.5
+model.length_min = 0.5
+model.length_max = 1.0
 
-B1 = model.add_box(b1)
-B2 = model.add_box(b2)
-# SPHERE = model.add_sphere(sphere)
-# CX = model.add_cylinder(cylinderx)
-# CY = model.add_cylinder(cylindery)
-# CZ = model.add_cylinder(cylinderz)
+BOX = model.add_box(box)
+SPHERE = model.add_sphere(sphere)
+CX = model.add_cylinder(cylinderx)
+CY = model.add_cylinder(cylindery)
+CZ = model.add_cylinder(cylinderz)
 
-# I = model.boolean_intersection(BOX, SPHERE)
-U = model.boolean_union(B1, B2)
-# D = model.boolean_difference(I, U)
+I = model.boolean_intersection(BOX, SPHERE)
+U = model.boolean_union(model.boolean_union(CX, CY), CZ)
+D = model.boolean_difference(I, U)
 
 model.generate_mesh(2)
 model.refine_mesh()
