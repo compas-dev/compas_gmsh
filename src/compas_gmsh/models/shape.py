b4ede7f57bb72e526d37bee709aba5b884c7e6f4
brgcode <vanmelet@ethz.ch>
2021-05-13 13:46:49 +0200

    new models

--- src/compas_gmsh/models/shape.py
+++ src/compas_gmsh/models/shape.py
@@ -4,12 +4,12 @@ import gmsh
 from compas.datastructures import Mesh
 
 
-class Model:
+class ShapeModel:
 
-    def __init__(self, name, verbose=False, algo=6):
+    def __init__(self, name):
         gmsh.initialize(sys.argv)
-        gmsh.option.setNumber("General.Terminal", int(verbose))
-        gmsh.option.setNumber("Mesh.Algorithm", algo)
+        gmsh.option.setNumber("General.Terminal", 0)
+        gmsh.option.setNumber("Mesh.Algorithm", 6)
         gmsh.model.add(name)
         self.mesh = gmsh.model.mesh
         self.factory = gmsh.model.occ
@@ -83,3 +83,62 @@ class Model:
             #     print(f'point: {len(etags)}')
 
         return Mesh.from_vertices_and_faces(xyz, triangles)
+
+    def add_cylinder(self, cylinder):
+        H = cylinder.height
+        R = cylinder.radius
+        base = cylinder.plane.point
+        normal = cylinder.plane.normal
+        start = base + normal.scaled(-0.5 * H)
+        vector = normal.scaled(H)
+        x0, y0, z0 = start
+        dx, dy, dz = vector
+        tag = self.factory.addCylinder(x0, y0, z0, dx, dy, dz, R)
+        # the dim is necessary because tags are only unique per dimension
+        return 3, tag  # return the dim and the tag
+
+    def add_sphere(self, sphere):
+        x, y, z = sphere.point
+        R = sphere.radius
+        tag = self.factory.addSphere(x, y, z, R)
+        return 3, tag
+
+    def add_box(self, box):
+        x0, y0, z0 = box.frame.point
+        x = x0 - 0.5 * box.xsize
+        y = y0 - 0.5 * box.ysize
+        z = z0 - 0.5 * box.zsize
+        tag = self.factory.addBox(x, y, z, box.xsize, box.ysize, box.zsize)
+        return 3, tag
+
+    def boolean_intersection(self, A, B):
+        """Boolean intersection of two shapes.
+
+        Parameters
+        ----------
+        A : tuple
+            The dimension and tag of the first shape.
+        B : tuple
+            The dimension and tag of the second shape.
+
+        Results
+        -------
+        tuple
+            The dimension and tag of the resulting shape.
+        """
+        result = self.factory.intersect([A], [B])
+        dimtags = result[0]
+        return dimtags[0]
+
+    def boolean_union(self, A, B):
+        result = self.factory.fuse([A], [B])
+        dimtags = result[0]
+        return dimtags[0]
+
+    def boolean_difference(self, A, B):
+        result = self.factory.cut([A], [B])
+        dimtags = result[0]
+        return dimtags[0]
+
+    def boolean_fragment(self, A, B):
+        pass
