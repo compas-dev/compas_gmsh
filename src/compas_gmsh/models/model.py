a7a5249c9c06588fc366fa2bfef1120a55c745a6
brgcode <vanmelet@ethz.ch>
2021-05-15 09:16:16 +0200

    export quads

--- src/compas_gmsh/models/model.py
+++ src/compas_gmsh/models/model.py
@@ -1,18 +1,26 @@
 from __future__ import annotations
 
+from typing import Optional
+
 import sys
 import gmsh
 
 from compas.datastructures import Mesh
 
+from compas_gmsh.options import MeshAlgorithm
+from compas_gmsh.options import OptimizationAlgorithm
+
 
 class Model:
     """Base model for mesh generation."""
 
-    def __init__(self, name: str, verbose: bool = False, algo: int = 6) -> Model:
+    def __init__(self,
+                 name: str,
+                 verbose: bool = False,
+                 algo: MeshAlgorithm = MeshAlgorithm.FrontalDelaunay) -> Model:
         gmsh.initialize(sys.argv)
         gmsh.option.setNumber("General.Terminal", int(verbose))
-        gmsh.option.setNumber("Mesh.Algorithm", algo)
+        gmsh.option.setNumber("Mesh.Algorithm", algo.value)
         gmsh.model.add(name)
         self.mesh = gmsh.model.mesh
         self.factory = gmsh.model.occ
@@ -57,8 +65,13 @@ class Model:
             print('--', local_node_coords)
             print('--', number_of_primary_nodes)
 
-    def generate_mesh(self, dim: int = 2) -> None:
+    def generate_mesh(self,
+                      dim: int = 2,
+                      verbose: bool = False,
+                      algo: MeshAlgorithm = MeshAlgorithm.FrontalDelaunay) -> None:
         """Generate a mesh of the current model."""
+        gmsh.option.setNumber("General.Terminal", int(verbose))
+        gmsh.option.setNumber("Mesh.Algorithm", algo.value)
         self.factory.synchronize()
         self.mesh.generate(dim)
 
@@ -66,36 +79,42 @@ class Model:
         """Refine the model mesh by uniformly splitting the edges."""
         self.mesh.refine()
 
-    # Literal["Netgen", "HighOrder", "HighOrderElastic", "HighOrderFastCurving"]
     def optimize_mesh(self,
-                      method: str = "",
+                      algo: Optional[OptimizationAlgorithm] = None,
                       niter: int = 1) -> None:
         """Optimize the model mesh using the specified method."""
-        self.mesh.optimize(method, niter=niter)
+        if algo:
+            algo = algo.value
+        else:
+            algo = ""
+        self.mesh.optimize(algo, niter=niter)
+
+    def recombine_mesh(self) -> None:
+        """Recombine the mesh into quadrilateral faces."""
+        self.mesh.recombine()
 
     def mesh_to_compas(self) -> Mesh:
         """Convert the model mesh to a COMPAS mesh data structure."""
         nodes = self.mesh.getNodes()
         node_tags = nodes[0]
         node_coords = nodes[1].reshape((-1, 3), order='C')
-        # node_paramcoords = nodes[2]
         xyz = {}
         for tag, coords in zip(node_tags, node_coords):
             xyz[int(tag)] = coords.tolist()
         elements = self.mesh.getElements()
-        triangles = []
+        faces = []
         for etype, etags, ntags in zip(*elements):
-            # if etype == 1:  # lines
-            #     print(f'line: {len(etags)}')
-            if etype == 2:  # triangles
-                # print(f'triangle: {len(etags)}')
+            if etype == 2:
+                # triangles
                 for i, etag in enumerate(etags):
                     n = self.mesh.getElementProperties(etype)[3]
                     triangle = ntags[i * n: i * n + n]
-                    triangles.append(triangle.tolist())
-            # elif etype == 4:  # tets
-            #     print(f'tetrahedron: {len(etags)}')
-            # elif etype == 15:  # points
-            #     print(f'point: {len(etags)}')
+                    faces.append(triangle.tolist())
+            elif etype == 3:
+                # quads
+                for i, etag in enumerate(etags):
+                    n = self.mesh.getElementProperties(etype)[3]
+                    quad = ntags[i * n: i * n + n]
+                    faces.append(quad.tolist())
 
-        return Mesh.from_vertices_and_faces(xyz, triangles)
+        return Mesh.from_vertices_and_faces(xyz, faces)
