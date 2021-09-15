abeb908a5247eaa5c9b429e1d4448da7eab47863
brgcode <vanmelet@ethz.ch>
2021-09-15 22:58:06 +0200

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_gmsh",
-    version="0.2.1rc0",
+    version="0.2.1",
     description="COMPAS friendly binding for GMSH",
     long_description=long_description,
     long_description_content_type="text/markdown",
